ccb0b60f76ff8ccf7d2c8beeca5e48a8ae82826b
Cristhian Atehortua <atecristian11@gmail.com>
2021-08-17 15:05:35 -0500

    create validateUser in folder middlware

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/user");
+const Auth = require("../middleware/auth");
+const ValidateUser = require("../middleware/validateUser");
 
 router.post("/registerUser", UserController.registerUser);
-router.get("/listUser/:name?", UserController.listUser); //lo colocamos asi cuando necesitamos un parametro en la url
+router.get("/listUser/:name?", Auth, ValidateUser, UserController.listUser); //lo colocamos asi cuando necesitamos un parametro en la url
 
 module.exports = router;
