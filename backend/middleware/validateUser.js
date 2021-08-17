3698da598dedec17de429b45bb0f9557eaa63bac
Cristhian Atehortua <atecristian11@gmail.com>
2021-08-17 15:26:27 -0500

    create validateUser in folder middlware

--- backend/middleware/validateUser.js
+++ backend/middleware/validateUser.js
@@ -1,9 +1,10 @@
 const User = require("../models/user");
 
 const user = async (req, res, next) => {
-    const user = await User.findById(req.user._id); //con findByIDpara se puedo mirar que el id que esta dentro del json del payload si sea el mismo que esta en nuestra bd 
-    if(!user) return res.status(400).send("Process failed: User without permission");
-    next();
+  const user = await User.findById(req.user._id); //con findByIDpara se puedo mirar que el id que esta dentro del json del payload si sea el mismo que esta en nuestra bd
+  if (!user)
+    return res.status(400).send("Process failed: User without permission");
+  next();
 };
 
 module.exports = user;
